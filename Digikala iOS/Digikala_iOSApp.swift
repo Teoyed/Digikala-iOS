f8f1c65f6f3ab776f37ba219b982f08bacc931e4
Reza Ahmadizadeh <mr.ahmadizadeh28@gmail.com>
2025-06-02 15:05:07 +0330

    Initial Commit

---
+++ Digikala iOS/Digikala_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Digikala_iOSApp.swift
+//  Digikala iOS
+//
+//  Created by Reza Ahmadizadeh on 3/12/1404 AP.
+//
+
+import SwiftUI
+
+@main
+struct Digikala_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
